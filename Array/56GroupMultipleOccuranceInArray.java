9b61ddeb90ed74da00a632531366aee55ef2c713
Anuj <80551063+codeanuj@users.noreply.github.com>
2021-08-19 01:17:01 +0530

    Create 56GroupMultipleOccuranceInArray.java

---
+++ Array/56GroupMultipleOccuranceInArray.java
@@ -0,0 +1,28 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	  int[] A = {4, 6, 9, 2, 3, 4, 9, 6, 10, 4};
+	 
+	  HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	  for(int i=0; i<A.length; i++){
+	      map.put(A[i], map.getOrDefault(A[i], 0)+1);
+	  }
+	  
+	  for(int i=0; i<A.length; i++){
+	      
+	      if(map.containsKey(A[i])){
+	          int freq = map.get(A[i]);
+	          
+	          while(freq!=0){
+	              System.out.println(A[i]);
+	          }
+	          
+	          map.remove(A[i]);
+	      }
+	  }
+	}
+}
