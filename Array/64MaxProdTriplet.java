f344fe59ba7a1d45cc6749c6973680539138dae0
Anuj <80551063+codeanuj@users.noreply.github.com>
2021-08-20 00:54:41 +0530

    Create 64MaxProdTriplet.java

---
+++ Array/64MaxProdTriplet.java
@@ -0,0 +1,45 @@
+ long maxProduct(int[] arr, int n) {
+        // code here
+        
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int max3=Integer.MIN_VALUE;
+        
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+        
+        
+        for(int i=0; i<arr.length; i++){
+            
+            if(arr[i]>max1){
+                max3=max2;
+                max2=max1;
+                max1=arr[i];
+            }
+            
+            else if(arr[i]>max2){
+                max3=max2;
+                max2=arr[i];
+            }
+            
+            else if(arr[i]>max3){
+                max3=arr[i];
+            }
+            
+            
+            if(arr[i]<min1){
+                min2=min1;
+                min1=arr[i];
+            }
+            
+            else if(arr[i]<min2){
+                min2=arr[i];
+            }
+            
+        }
+        
+        long p1 =max1*max2*max3;
+        long p2 =min1*min2*max1;
+        return Math.max(p1, p2);
+ 
+    }
