2e199588438e17946363a028363b40b7ce0542f0
Anuj <80551063+codeanuj@users.noreply.github.com>
2022-01-05 00:25:51 +0530

    Update 17ReverseWhileIncludingZero.java

--- BitManupulation/17ReverseWhileIncludingZero.java
+++ BitManupulation/17ReverseWhileIncludingZero.java
@@ -3,8 +3,9 @@
         
         int rev=0;
         int pos=31;
+    
         while(n!=0 && pos>=0){
-            
+            //if lsb is 1 shift it left with position and takr or with answer(rev)
             if((n&1)!=0){
                rev =rev|(1<<pos); 
             }
